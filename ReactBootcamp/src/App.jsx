d89ace896cc3f00517dcaa19943a490c2d2ebd2e
DeeptiSharmaCG <deepti.sharma@cginfinity.com>
2026-07-09 21:51:38 +0530

    Initial commit

---
+++ ReactBootcamp/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter , Routes , Route, Link} from 'react-router-dom'
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  function Increment() {
+    setCount((prev) => prev + 1);
+  }
+  function Decrement() {
+    setCount((prev) => prev - 1);
+  }
+  
+
+  return (
+    <div>
+
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/navbar">Navbar</Link>
+      </nav>
+
+      <Routes>
+        
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Counter App</h1>
+              <h2>{count}</h2>
+              <button onClick={Increment}>Increment</button>
+              <button onClick={Decrement}>Decrement</button>
+            </div>
+          }
+        />
+        <Route path="/navbar" element={<Navbar name="My navbar" />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ ReactBootcamp/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function Navbar(prop) {
+  return (
+    <div>
+        <Link to="/test">Test</Link>
+        <h1>{prop.name}</h1>
+
+    </div>
+  )
+}
+
+export default Navbar
+
